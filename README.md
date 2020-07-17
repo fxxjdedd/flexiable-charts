e57d244dba8a4eeec3819283b65f9d66f5a5db82
fxxjdedd <506036406@qq.com>
2020-07-17 13:22:17 +0800

    feat: 支持多个GeneratorService

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # flexiable-charts
+
 Created with CodeSandbox
+
+# todos
+
+- [] 全局注册，局部注册，全局兼容性注册（既要大规模应用插件，也要保证兼容性）
+- [] generator service 注册多个？
+- [] generator 还是不够抽象
+- [] 写 react、vue 时支持 jsx
---
+++ src/util.ts
@@ -0,0 +1,36 @@
+import { DataSource } from "./service";
+import { isValidElement as isValidReactElement } from "react";
+import { VNode } from "vue";
+
+export function isValidVueElement(mayBeVNode: object): mayBeVNode is VNode {
+  return (
+    mayBeVNode &&
+    ["tag", "data", "children", "text", "elm", "ns", "context", "key"].every(
+      key => key in mayBeVNode
+    )
+  );
+}
+
+export function isDataSource(
+  mayBeDataSource: any
+): mayBeDataSource is DataSource {
+  return ["id", "from"].every(key => key in mayBeDataSource);
+}
+
+export function isAllElement(arr: any[]): arr is any {
+  return arr.every(
+    item => isValidReactElement(item) || isValidVueElement(item)
+  );
+}
+
+export function isAllObject(arr: any[]): arr is object[] {
+  return arr.every(item => typeof item === "object");
+}
+
+export function isAllFunction(arr: any[]): arr is Function[] {
+  return arr.every(item => typeof item === "function");
+}
+
+export function assertUnreachable(msg?: string): never {
+  throw new Error(msg);
+}
